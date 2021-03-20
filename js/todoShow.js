a85b48c9d3e45243b7f6fa90910572cb5c50f26b
thereisname <79213859+thereisname@users.noreply.github.com>
2021-03-21 04:28:14 +0900

    Design

---
+++ js/todoShow.js
@@ -0,0 +1,9 @@
+const todo = document.querySelector(".todo"). bars = document.querySelector(".bars");
+ 
+
+function init() {
+    const hide = todo.querySelector("button");
+    console.log(hide)
+}
+
+init();
